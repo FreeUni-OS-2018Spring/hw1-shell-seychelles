cab45b5fa9b533b713fcc9b28924e95f83b9783d
tkhac <tkhac14@freeuni.edu.ge>
2018-03-17 23:16:07 +0400

    Pipe Support
    Added pipe redirection support, with some minor changes. There are memory leaks which should be fixed

--- shell.c
+++ shell.c
@@ -27,13 +27,13 @@ struct termios shell_tmodes;
 /* Process group id for the shell */
 pid_t shell_pgid;
 
-int cmd_exit(struct command *tokens);
-int cmd_help(struct command *tokens);
-int cmd_pwd(struct command *tokens);
-int cmd_cd(struct command *tokens);
+int cmd_exit(char** command);
+int cmd_help(char** command);
+int cmd_pwd(char** command);
+int cmd_cd(char** command);
 
 /* Built-in command functions take token array (see parse.h) and return int */
-typedef int cmd_fun_t(struct command *tokens);
+typedef int cmd_fun_t(char** command);
 
 /* Built-in command struct and lookup table */
 typedef struct fun_desc {
@@ -50,19 +50,19 @@ fun_desc_t cmd_table[] = {
 };
 
 /* Prints a helpful description for the given command */
-int cmd_help(unused struct command *tokens) {
+int cmd_help(unused char** command) {
   for (unsigned int i = 0; i < sizeof(cmd_table) / sizeof(fun_desc_t); i++)
     printf("%s - %s\n", cmd_table[i].cmd, cmd_table[i].doc);
   return 1;
 }
 
 /* Exits this shell */
-int cmd_exit(unused struct command *tokens) {
+int cmd_exit(unused char** command) {
   exit(0);
 }
 
 /* Prints working directory */
-int cmd_pwd(unused struct command *tokens) {
+int cmd_pwd(unused char** command) {
   char cwd[1024];
   if (getcwd(cwd, sizeof(cwd)) != NULL)
     fprintf(stdout, "%s\n", cwd);
@@ -72,8 +72,8 @@ int cmd_pwd(unused struct command *tokens) {
 }
 
 /* Changes working directory */
-int cmd_cd(struct command *tokens) {
-  char* path = commands_get_cmd(tokens, 0)[1];
+int cmd_cd(char** command) {
+  char* path = command[1]; // need error checking.
   if(chdir(path) != 0) {
     fprintf(stdout, "cd: %s: No such file or directory\n", path);
   }
@@ -88,27 +88,72 @@ int lookup(char cmd[]) {
   return -1;
 }
 
-int execute_command(struct command* command) {
+int redirected_execution(struct command* command) {
   char** args;
-  if (commands_get_length(command) > 1) {
-    // Pipes Handling
-  } else {
-    args = commands_get_cmd(command, 0);
-    if (access(args[0], 0) < 0) { // check if progrm exists.
-      return -1;
-    }
-    pid_t pid;
-    pid = fork(); /* fork a child process */
-    if (pid < 0) { /* error, Fork Failed */
-      return -1;
-    } else if (pid == 0) { /* child process */
+  size_t commands_count = commands_get_length(command);
+
+  /* Prepare Pipes, they are not deallocated. */
+  int* pipe_fds[commands_count - 1];
+  for (int i = 0; i < commands_count - 1; i++) {
+    int* f_des = malloc(2 * sizeof(int)); // Read from 0 write to 1.
+    pipe(f_des);
+    pipe_fds[i] = f_des;
+  }
+  
+  for (size_t i = 0; i < commands_count; i++) {
+    args = commands_get_cmd(command, i);
+
+    pid_t pid = fork();
+    if (pid < 0) {
+      return -2;
+    } else if (pid == 0) { /* Child Process */
+      /* Here all unused file descriptors must be closed, but are not in this code. */
+      if (i == 0) { /* First process, only writes to pipe. */
+        dup2(pipe_fds[0][1], 1);
+        // Here can be file input redirect.
+      } else if (i == commands_count - 1) { /* Last process, only reads from pipe. */
+        dup2(pipe_fds[i - 1][0], 0);
+        // Here can be file output redirect.
+      } else { /* Middle process, reads from pipe and writes to next pipe. */
+        dup2(pipe_fds[i - 1][0], 0);
+        dup2(pipe_fds[i][1], 1);
+      }
       execv(args[0], args);
-      exit(1); // if exec fails process dies.
-    } else { /* parent process */
-      wait(NULL);
+      exit(1);
+    } else { /* Parent Process */
+      if (i < commands_count - 1) {
+        close(pipe_fds[i][1]); // Close current write.
+      }
+      waitpid(pid, NULL, 0);
+      if (i > 0) {
+        close(pipe_fds[i - 1][0]); // Close previous read.
+      }
     }
   }
   return 0;
+}
+
+int execute_command(char** command) {
+  int status = 1;
+  int fundex = lookup(command[0]);
+  if (fundex >= 0) {
+    status = cmd_table[fundex].fun(command);
+  } else {
+    char* program = command[0];
+    if (access(program, 0) < 0) { /* Check if program exists */
+      return -1; /* Does Not Exists */
+    }
+    pid_t pid = fork(); /* Create a child process */
+    if (pid < 0) {
+      return -2; /* Fork Failed error */
+    } else if (pid == 0) { /* Child Process */
+      execv(program,command);
+      exit(1); // if exec fails process dies.
+    } else { /* Parent Process */
+      wait(&status);
+    }
+  }
+  return status;
 }
 
 /* Intialization procedures for this shell */
@@ -152,18 +197,20 @@ int main(unused int argc, unused char *argv[]) {
     struct command* command = parse(line);
     char** first_subcmd = commands_get_cmd(command, 0);
     
-    if (strcmp(line, "\n") && first_subcmd != NULL) {
-      
-      /* Find which built-in function to run. */
-      int fundex = lookup(first_subcmd[0]);
-      if (fundex >= 0) {
-        cmd_table[fundex].fun(command);
-      } else {
-        /* REPLACE this to run commands as programs. */
-        int status = execute_command(command);
+    if (strcmp(line, "\n")) {
+      if (first_subcmd != NULL) {
+        /* Find which built-in function to run. */
+        int status;
+        if (commands_get_length(command) > 1) { // Pipes Handling
+          status = redirected_execution(command);
+        } else {
+          status = execute_command(first_subcmd);
+        }
         if (status != 0) {
           fprintf(stdout, "This shell doesn't know how to run programs.\n");
         }
+      } else {
+        fprintf(stdout, "Syntax error!\n");
       }
     }
 
